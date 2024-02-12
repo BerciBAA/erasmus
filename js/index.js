d1ab778b6d6c798aa715e13c1837f914db5346de
BerciBAA <bercike222@gmail.com>
2024-02-12 12:03:25 +0100

    addig index.js and index.php

--- js/index.js
+++ js/index.js
@@ -2,4 +2,21 @@ document.addEventListener('DOMContentLoaded', function(){
     if (!localStorage.getItem('userLoggedIn')) {
         window.location.href = 'login.html';
     }
+
+    fetch(`${config.API_BASE_URL}/php/index.php`, {
+        method: 'GET',
+        credentials: 'include'
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            console.log('User ID:', data.user_id);
+            document.querySelector('.container').insertAdjacentHTML('beforeend', `<p>User ID: ${data.user_id}</p>`);
+        } else {
+            console.error('Error or user not logged in:', data.message);
+        }
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
 });
